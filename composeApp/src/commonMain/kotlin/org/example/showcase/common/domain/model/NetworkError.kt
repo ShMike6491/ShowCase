a1848b8a461914a82be836afc187d58fb7f244ea
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-24 16:23:43 +0300

    [ADDED] common network error interface

---
+++ composeApp/src/commonMain/kotlin/org/example/showcase/common/domain/model/NetworkError.kt
@@ -0,0 +1,13 @@
+package org.example.showcase.common.domain.model
+
+enum class NetworkError : IError {
+    REQUEST_TIMEOUT,
+    UNAUTHORIZED,
+    CONFLICT,
+    TOO_MANY_REQUESTS,
+    NO_INTERNET,
+    PAYLOAD_TOO_LARGE,
+    SERVER_ERROR,
+    SERIALIZATION,
+    UNKNOWN;
+}
