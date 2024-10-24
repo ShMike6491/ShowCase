79f964176a5e732d47aff6d0e5ce34ac1197b70c
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-24 17:18:50 +0300

    [CHANGED] ios di initialization update

--- composeApp/src/iosMain/kotlin/org/example/showcase/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/showcase/MainViewController.kt
@@ -1,5 +1,11 @@
 package org.example.showcase
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.example.showcase.app.di.DependencyInjection
+import org.example.showcase.di.iosModule
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = { DependencyInjection.initKoin(iosModule) }
+) {
+    App()
+}
