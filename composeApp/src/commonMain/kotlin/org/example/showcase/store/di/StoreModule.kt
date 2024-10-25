0a4185d613636bbf3a04b6d97f2162b78a507f92
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 16:22:50 +0300

    [CHANGED] store di implementation update

--- composeApp/src/commonMain/kotlin/org/example/showcase/store/di/StoreModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/di/StoreModule.kt
@@ -1,22 +1,21 @@
 package org.example.showcase.store.di
 
+import com.arkivanov.mvikotlin.core.store.SimpleBootstrapper
 import com.arkivanov.mvikotlin.core.store.Store
-import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import com.arkivanov.mvikotlin.core.store.StoreFactory
 import org.example.showcase.common.ui.model.UiIntent
 import org.example.showcase.store.data.StoreHttpKtorClient
 import org.example.showcase.store.data.StoreRepositoryImpl
 import org.example.showcase.store.domain.StoreProductsInteractor
 import org.example.showcase.store.domain.repository.IStoreRepository
 import org.example.showcase.store.ui.details.DETAILS_STORE_TAG
-import org.example.showcase.store.ui.details.DetailsBootstrapperImpl
 import org.example.showcase.store.ui.details.DetailsExecutorImpl
 import org.example.showcase.store.ui.details.DetailsReducerImpl
 import org.example.showcase.store.ui.details.DetailsStore
+import org.example.showcase.store.ui.feed.FEED_STORE_TAG
 import org.example.showcase.store.ui.feed.FeedExecutorImpl
 import org.example.showcase.store.ui.feed.FeedReducerImpl
 import org.example.showcase.store.ui.feed.FeedStore
-import org.example.showcase.store.ui.feed.FEED_STORE_TAG
-import org.example.showcase.store.ui.feed.FeedBootstrapperImpl
 import org.example.showcase.store.ui.model.ProductDetailState
 import org.example.showcase.store.ui.model.ProductFeedState
 import org.koin.dsl.module
@@ -29,26 +28,25 @@ val storeFeatureModule = module {
     single { StoreProductsInteractor(get()) }
 
     single<FeedStore> {
-        val storeFactory = DefaultStoreFactory()
-        val interactor: StoreProductsInteractor = get()
+        val storeFactory: StoreFactory = get()
         object : FeedStore, Store<UiIntent, ProductFeedState, Nothing> by storeFactory.create(
             name = FEED_STORE_TAG,
             initialState = ProductFeedState(),
-            bootstrapper = FeedBootstrapperImpl(interactor),
+            bootstrapper = SimpleBootstrapper(Unit),
             reducer = FeedReducerImpl,
             executorFactory = { FeedExecutorImpl() }
         ) {}
     }
 
-    single<DetailsStore> {
-        val storeFactory = DefaultStoreFactory()
-        val interactor: StoreProductsInteractor = get()
+    factory<DetailsStore> { params ->
+        val itemId: String = params.get()
+        val storeFactory: StoreFactory = get()
         object : DetailsStore, Store<UiIntent, ProductDetailState, Nothing> by storeFactory.create(
             name = DETAILS_STORE_TAG,
             initialState = ProductDetailState(),
-            bootstrapper = DetailsBootstrapperImpl(interactor),
+            bootstrapper = SimpleBootstrapper(Unit),
             reducer = DetailsReducerImpl,
-            executorFactory = { DetailsExecutorImpl() }
+            executorFactory = { DetailsExecutorImpl(itemId) }
         ) {}
     }
 }
