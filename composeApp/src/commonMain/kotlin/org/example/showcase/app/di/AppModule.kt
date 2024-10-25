7f08206dac5428129e3b6b3c1f93f6fb7b9573b0
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 16:19:57 +0300

    [CHANGED] store factory dependency update

--- composeApp/src/commonMain/kotlin/org/example/showcase/app/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/app/di/AppModule.kt
@@ -1,8 +1,12 @@
 package org.example.showcase.app.di
 
+import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
 import org.koin.dsl.module
 
 val appModule = module {
 
     single { createHttpClient(engine = get()) }
+
+    single<StoreFactory> { DefaultStoreFactory() }
 }
