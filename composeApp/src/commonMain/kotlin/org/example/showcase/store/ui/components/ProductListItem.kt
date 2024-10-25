338109f6aef0d69239b8ce60480f62007eb20d70
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 20:52:06 +0300

    [CHANGED] image loading layout mapping update

--- composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/components/ProductListItem.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/components/ProductListItem.kt
@@ -1,7 +1,6 @@
 package org.example.showcase.store.ui.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
@@ -23,8 +21,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,11 +37,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.coil3.CoilImage
 import org.example.showcase.common.ui.model.UiIntent
 import org.example.showcase.store.ui.model.ProductItemState
-import org.jetbrains.compose.resources.painterResource
-import showcase.composeapp.generated.resources.Res
-import showcase.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun ProductListItem(
@@ -45,6 +48,9 @@ fun ProductListItem(
     state: ProductItemState,
     action: (UiIntent) -> Unit
 ) {
+    var likedState: Boolean by remember { mutableStateOf(state.isLiked) }
+    val vectorRes = if (likedState) Icons.Default.Favorite else Icons.Default.FavoriteBorder
+
     Card(
         shape = RoundedCornerShape(20.dp),
         modifier = modifier.fillMaxWidth()
@@ -56,31 +62,45 @@ fun ProductListItem(
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            // todo: like click animation?
             IconButton(
                 modifier = Modifier
                     .testTag("itemSideIconButton")
                     .size(24.dp),
-                onClick = { /* todo: handle ui events via intent */ }
+                onClick = {
+                    val updatedState = !likedState
+                    likedState = updatedState
+                    action(UiIntent.ToggleChange(updatedState, state.id))
+                }
             ) {
                 Icon(
                     modifier = Modifier.testTag("itemSideIcon"),
-                    //Icons.Default.Favorite - this is filled resource
-                    imageVector = Icons.Default.FavoriteBorder,
+                    imageVector = vectorRes,
                     contentDescription = "Add to favorites",
                     tint = MaterialTheme.colors.primary
                 )
             }
 
-            // todo: load image from url
-            Image(
-                painter = painterResource(Res.drawable.compose_multiplatform),
-                contentDescription = "Card Image",
-                contentScale = ContentScale.Crop,
+            CoilImage(
+                imageModel = { state.imageUrl },
+                imageOptions = ImageOptions(
+                    contentDescription = "Card Image",
+                    contentScale = ContentScale.Inside,
+                    alignment = Alignment.Center
+                ),
+                failure = {
+                    Icon(
+                        imageVector = Icons.Default.Image,
+                        contentDescription = "Failed Load Image",
+                        modifier = Modifier
+                            .testTag("itemFailedImage")
+                            .size(40.dp)
+                            .align(Alignment.Center)
+                    )
+                },
                 modifier = Modifier
                     .testTag("itemImage")
                     .size(100.dp)
-                    .clip(CircleShape)
+                    .clip(RoundedCornerShape(percent = 50))
                     .background(Color.White)
                     .align(Alignment.CenterVertically)
             )
@@ -128,7 +148,6 @@ fun ProductListItem(
                             contentColor = MaterialTheme.colors.primary,
                             backgroundColor = Color.Transparent
                         ),
-                        // todo: use better event handling?
                         onClick = { action(UiIntent.ButtonClick(state.id)) }
                     ) {
                         Text(
--- composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/details/DetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/details/DetailsScreen.kt
@@ -1,6 +1,5 @@
 package org.example.showcase.store.ui.details
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
@@ -22,8 +20,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,11 +37,10 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.coil3.CoilImage
 import org.example.showcase.common.ui.model.UiIntent
 import org.example.showcase.store.ui.model.ProductDetailState
-import org.jetbrains.compose.resources.painterResource
-import showcase.composeapp.generated.resources.Res
-import showcase.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 fun DetailScreen(component: DetailComponent) {
@@ -79,7 +82,7 @@ private fun Content(
             }
 
             else -> Column(modifier = Modifier.padding(16.dp)) {
-                Header(state.imageUrl) { action(it) }
+                Header(state) { action(it) }
                 Spacer(modifier = Modifier.height(24.dp))
 
                 if (state.titleText != null) {
@@ -136,9 +139,12 @@ private fun Content(
 
 @Composable
 fun Header(
-    imageUrl: String? = null,
+    state: ProductDetailState,
     action: (UiIntent) -> Unit
 ) {
+    var likedState: Boolean by remember { mutableStateOf(state.isLiked) }
+    val vectorRes = if (likedState) Icons.Default.Favorite else Icons.Default.FavoriteBorder
+
     Card(
         shape = RoundedCornerShape(20.dp),
         modifier = Modifier
@@ -168,34 +174,45 @@ fun Header(
                 )
             }
 
-            if (imageUrl != null) {
-                // todo: move image here
+            if (state.imageUrl != null) {
+                CoilImage(
+                    imageModel = { state.imageUrl },
+                    imageOptions = ImageOptions(
+                        contentDescription = "Card Image",
+                        contentScale = ContentScale.Inside,
+                        alignment = Alignment.Center
+                    ),
+                    failure = {
+                        Icon(
+                            imageVector = Icons.Default.Image,
+                            contentDescription = "Failed Load Image",
+                            modifier = Modifier
+                                .size(40.dp)
+                                .align(Alignment.Center)
+                        )
+                    },
+                    modifier = Modifier
+                        .testTag("itemImage")
+                        .size(150.dp)
+                        .clip(RoundedCornerShape(percent = 50))
+                        .background(Color.White)
+                        .align(Alignment.CenterVertically)
+                )
             }
 
-            // todo: load image from url
-            Image(
-                painter = painterResource(Res.drawable.compose_multiplatform),
-                contentDescription = "Card Image",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .testTag("itemImage")
-                    .size(150.dp)
-                    .clip(CircleShape)
-                    .background(Color.White)
-                    .align(Alignment.CenterVertically)
-            )
-
-            // todo: like click animation?
             IconButton(
                 modifier = Modifier
                     .testTag("headerActionIconButton")
                     .size(24.dp),
-                onClick = { /* todo: handle like click */ }
+                onClick = {
+                    val updatedState = !likedState
+                    likedState = updatedState
+                    action(UiIntent.ToggleChange(updatedState, state.id))
+                }
             ) {
                 Icon(
                     modifier = Modifier.testTag("headerActionIcon"),
-                    //Icons.Default.Favorite - this is filled resource
-                    imageVector = Icons.Default.FavoriteBorder,
+                    imageVector = vectorRes,
                     // todo: hardcoded description
                     contentDescription = "Add to favorites",
                     tint = MaterialTheme.colors.primary
