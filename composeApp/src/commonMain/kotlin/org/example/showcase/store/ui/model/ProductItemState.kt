ce85b597a0a18b967ff8b6d199d4ac0e608c72c9
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 10:23:27 +0300

    [CHANGED] ui item state configurations change

--- composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/model/ProductItemState.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/model/ProductItemState.kt
@@ -6,26 +6,17 @@ import org.example.showcase.common.ui.model.UiNumber
 import org.example.showcase.common.ui.model.UiString
 import org.example.showcase.common.ui.model.asUiString
 import org.example.showcase.store.domain.model.IProduct
-import kotlin.math.pow
-import kotlin.math.roundToInt
-
-// todo: add icon vector resource
 
 data class ProductItemState(
     override val id: String,
     val titleText: UiString,
     val price: UiNumber,
     val imageUrl: String,
-
     //todo: change hardcoded
     val actionText: UiString = "Buy".asUiString(),
-    val primaryAction: (() -> Unit)? = null,
-    val sideAction: (() -> Unit)? = null,
 ) : IIdentifiable
 
 fun IProduct.asProductItemState(
-    primaryAction: (() -> Unit)? = null,
-    sideAction: (() -> Unit)? = null
 ): ProductItemState {
     val roundedPriceText = priceUsd.roundTo(2)
         .toString()
@@ -34,8 +25,6 @@ fun IProduct.asProductItemState(
         id = id,
         titleText = title.asUiString(),
         price = UiNumber(priceUsd, roundedPriceText),
-        imageUrl = imageUrl,
-        primaryAction = primaryAction,
-        sideAction = sideAction
+        imageUrl = imageUrl
     )
 }
