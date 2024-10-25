f96c623713977c211fee0be3425343e0dee41b42
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 16:34:52 +0300

    [CHANGED] ios navigation initialization update

--- composeApp/src/iosMain/kotlin/org/example/showcase/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/showcase/MainViewController.kt
@@ -1,11 +1,19 @@
 package org.example.showcase
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import org.example.showcase.app.di.DependencyInjection
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import org.example.showcase.app.ui.App
 import org.example.showcase.di.iosModule
 
 fun MainViewController() = ComposeUIViewController(
     configure = { DependencyInjection.initKoin(iosModule) }
 ) {
-    App()
+    val root = remember {
+        RootComponent(DefaultComponentContext(LifecycleRegistry()))
+    }
+
+    App(root)
 }
