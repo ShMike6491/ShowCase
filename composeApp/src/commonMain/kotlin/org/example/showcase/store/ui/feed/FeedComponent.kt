48c2f2e2212093e411e4a9a918cec4495af32863
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 16:31:31 +0300

    [ADDED] feed page component

---
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/ui/feed/FeedComponent.kt
@@ -0,0 +1,30 @@
+package org.example.showcase.store.ui.feed
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import org.example.showcase.common.navigation.INavigator
+import org.example.showcase.common.navigation.NavRoute
+import org.example.showcase.common.ui.asValue
+import org.example.showcase.common.ui.model.UiIntent
+import org.example.showcase.store.ui.model.ProductFeedState
+import org.koin.core.component.KoinComponent
+
+class FeedComponent(
+    private val componentContext: ComponentContext,
+    private val navigator: INavigator,
+): KoinComponent, ComponentContext by componentContext {
+
+    private val store: FeedStore = instanceKeeper.getStore { getKoin().get() }
+
+    val model: Value<ProductFeedState> = store.asValue()
+
+    // todo: improve intent handling
+    fun onIntent(intent: UiIntent) {
+//        store.accept(intent)
+        if (intent is UiIntent.ButtonClick && intent.id != null) {
+            val route = NavRoute.DetailRoute(intent.id)
+            navigator.navigateTo(route)
+        }
+    }
+}
