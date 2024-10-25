6763eddfa1466a545034d32e478173fd28726894
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 17:35:16 +0300

    [CHANGED] network error bugfix

--- composeApp/src/commonMain/kotlin/org/example/showcase/store/data/StoreHttpKtorClient.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/data/StoreHttpKtorClient.kt
@@ -3,55 +3,63 @@ package org.example.showcase.store.data
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.serialization.ContentConvertException
 import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.io.IOException
 import kotlinx.serialization.SerializationException
 import org.example.showcase.common.domain.model.IError
 import org.example.showcase.common.data.model.NetworkError
 import org.example.showcase.store.data.dto.ProductDto
 import org.example.showcase.common.domain.model.Result
+import org.example.showcase.store.data.dto.asDataModel
 import org.example.showcase.store.domain.model.IProduct
 
-// fixme: app crushes on any network error
 class StoreHttpKtorClient(private val httpClient: HttpClient) {
 
     suspend fun getAllProducts(): Result<List<IProduct>, IError> {
-        val response = try {
-            httpClient.get(urlString = API_ENDPOINT)
-        } catch(e: UnresolvedAddressException) {
-            return Result.Error(NetworkError.NO_INTERNET)
-        } catch(e: SerializationException) {
-            return Result.Error(NetworkError.SERIALIZATION)
-        }
-
-        // todo: make an extension?
-        return when(response.status.value) {
-            in 200..299 -> {
-                val body = response.body<List<ProductDto>>()
-                Result.Success(body)
+        return httpRequest(API_ENDPOINT) { response ->
+            response.handleResponse {
+                response.body<List<ProductDto>>()
+                    .map { it.asDataModel() }
             }
-            401 -> Result.Error(NetworkError.UNAUTHORIZED)
-            409 -> Result.Error(NetworkError.CONFLICT)
-            408 -> Result.Error(NetworkError.REQUEST_TIMEOUT)
-            413 -> Result.Error(NetworkError.PAYLOAD_TOO_LARGE)
-            in 500..599 -> Result.Error(NetworkError.SERVER_ERROR)
-            else -> Result.Error(NetworkError.UNKNOWN)
         }
     }
 
     suspend fun getProductById(id: String): Result<IProduct, IError> {
-        val response = try {
-            httpClient.get(urlString = "$API_ENDPOINT/$id")
-        } catch(e: UnresolvedAddressException) {
-            return Result.Error(NetworkError.NO_INTERNET)
-        } catch(e: SerializationException) {
-            return Result.Error(NetworkError.SERIALIZATION)
+        return httpRequest("$API_ENDPOINT/$id") { response ->
+            response.handleResponse {
+                response.body<ProductDto>().asDataModel()
+            }
         }
+    }
 
-        // todo: make an extension?
-        return when(response.status.value) {
+    private suspend fun <T> httpRequest(
+        url: String,
+        request: suspend (HttpResponse) -> Result<T, IError>
+    ): Result<T, IError> {
+        return try {
+            val response = httpClient.get(urlString = url)
+            request(response)
+        } catch (e: UnresolvedAddressException) {
+            Result.Error(NetworkError.NO_INTERNET)
+        } catch (e: SerializationException) {
+            Result.Error(NetworkError.SERIALIZATION)
+        }  catch (e: IOException) {
+            Result.Error(NetworkError.NO_INTERNET)
+        }
+    }
+
+    private suspend inline fun <T> HttpResponse.handleResponse(
+        successHandler: (HttpResponse) -> T
+    ): Result<T, IError> {
+        return when (status.value) {
             in 200..299 -> {
-                val body = response.body<ProductDto>()
-                Result.Success(body)
+                try {
+                    Result.Success(successHandler(this))
+                } catch (e: ContentConvertException) {
+                    Result.Error(NetworkError.CONFLICT)
+                }
             }
             401 -> Result.Error(NetworkError.UNAUTHORIZED)
             409 -> Result.Error(NetworkError.CONFLICT)
