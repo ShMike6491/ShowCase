faae364f78b77f253bd4caa43f92759988835000
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 10:25:06 +0300

    [ADDED] string util common api

---
+++ composeApp/src/commonMain/kotlin/org/example/showcase/common/domain/StringUtils.kt
@@ -0,0 +1,8 @@
+package org.example.showcase.common.domain
+
+fun getRandomString(length: Int = 25) : String {
+    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    return (1..length)
+        .map { allowedChars.random() }
+        .joinToString("")
+}
