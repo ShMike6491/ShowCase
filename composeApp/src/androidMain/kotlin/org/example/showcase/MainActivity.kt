6c2ce989885770339100499d4f60048bd79c3119
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 16:34:45 +0300

    [CHANGED] main activity navigation initialization update

--- composeApp/src/androidMain/kotlin/org/example/showcase/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/showcase/MainActivity.kt
@@ -3,13 +3,18 @@ package org.example.showcase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.arkivanov.decompose.retainedComponent
+import org.example.showcase.app.navigation.RootComponent
+import org.example.showcase.app.ui.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val root = retainedComponent { RootComponent(it) }
+
         setContent {
-            App()
+            App(root)
         }
     }
 }
