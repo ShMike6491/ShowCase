9c3556bc7e4e4f2dd6e26bab2032007974e62f43
michaelshilovski <mikhail.shilovsky@flexsentlabs.com>
2024-10-25 17:34:58 +0300

    [CHANGED] serialization quick bugfix

--- composeApp/src/commonMain/kotlin/org/example/showcase/store/data/dto/ProductDto.kt
+++ composeApp/src/commonMain/kotlin/org/example/showcase/store/data/dto/ProductDto.kt
@@ -2,25 +2,23 @@ package org.example.showcase.store.data.dto
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 import org.example.showcase.store.domain.model.IProduct
 
 @Serializable
 data class ProductDto(
     @SerialName("id") val idNum : Int,
-    @SerialName("title") override val title: String,
+    @SerialName("title") val titleName: String,
     @SerialName("price") val price: Float,
-    @SerialName("category") override val category: String,
-    @SerialName("description") override val description: String,
+    @SerialName("category") val categoryName: String,
+    @SerialName("description") val descriptionText: String,
     @SerialName("image") val image: String
-) : IProduct {
+)
 
-    @Transient
-    override val id: String = idNum.toString()
-
-    @Transient
+fun ProductDto.asDataModel() = object : IProduct {
+    override val title: String = titleName
     override val priceUsd: Double = price.toDouble()
-
-    @Transient
-    override val imageUrl: String  = image
+    override val category: String = categoryName
+    override val description: String = descriptionText
+    override val imageUrl: String = image
+    override val id: String = idNum.toString()
 }
